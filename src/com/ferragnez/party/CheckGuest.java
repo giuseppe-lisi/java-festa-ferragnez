eb800a93a2f49b87f6e0c8ccbccab040ba0e57b3
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-04-08 13:33:13 +0200

    implementato check per ammettere o meno un guest con ciclo for

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,7 +1,28 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
     public static void main() {
-        System.out.println("hello java");
+        
+        // invitare totti e la blasi alla stessa festa è veramente criminale
+        String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+
+        // creo istanza scanner
+        Scanner in = new Scanner(System.in);
+        
+        // chiedo all'utente il suo nome
+        System.out.println("Inserisci il tuo nome: ");
+        String guestName = in.nextLine();
+
+        boolean canEnter = false;
+        for (int i = 0; i < guestList.length; i++) {
+            if (guestName.equals(guestList[i])) {
+                canEnter = true;
+                break;
+            }
+        }
+
+        System.out.println(canEnter ? "Sei in lista, puoi entrare" : "Mi dispiace, tornatene a casa");
     }
 }
